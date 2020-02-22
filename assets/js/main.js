3172ab5071e3d50fa70644b5e77e35e907b6a050
Jose Garfias Lopez <garfiaslopez@hotmail.com>
2020-02-22 08:28:29 -0600

    some changes on work carusel

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,6 +83,43 @@ $(function () {
         time: 3000
     });
 
+    //===== our work active slider
+
+    $('.slider-work').slick({
+        dots: true,
+        speed: 800,
+        arrows: false,
+        centerMode: true,
+        centerPadding: "0",
+        slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
 
     //===== testimonial active
 
